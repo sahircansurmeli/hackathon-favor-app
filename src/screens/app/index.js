3775922e9b414149ac8adf95f6b9c824c672f133
Enes Tasbasi <enestasbasi@outlook.com>
2021-04-17 23:37:27 -0400

    Disable warnings

--- src/screens/app/index.js
+++ src/screens/app/index.js
@@ -7,23 +7,33 @@ import AddBook from "./AddBook";
 import AddSkill from "./AddSkill";
 import ProfileScreen from "./ProfileScreen";
 import ExchangeScreen from "./ExchangeScreen";
+import { LogBox } from "react-native";
+
+LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 const Stack = createStackNavigator();
 
 export default function AuthContainer() {
   return (
-    <Stack.Navigator initialRouteName="Home" screenOptions={{
-      headerTransparent: true,
-      headerTitle: () => null,
-      headerBackTitleVisible: false,
-      headerTintColor: "#FFFFFF"
-    }}>
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerTransparent: true,
+        headerTitle: () => null,
+        headerBackTitleVisible: false,
+        headerTintColor: "#FFFFFF",
+      }}
+    >
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Leaderboard" component={LeaderboardScreen} />
       <Stack.Screen name="AddBook" component={AddBook} />
       <Stack.Screen name="AddSkill" component={AddSkill} />
       <Stack.Screen name="Exchanges" component={ExchangeScreen} />
-      <Stack.Screen name="Profile" component={ProfileScreen} options={{ headerTintColor: "#000" }} />
+      <Stack.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{ headerTintColor: "#000" }}
+      />
     </Stack.Navigator>
   );
 }
